e8f5ba1f3f08f9bb9f0ca59b16e8efb43f16ffdc
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-11-23 14:02:57 -0200

    Initial commit

---
+++ app/src/main/java/io/edanni/money/domain/entity/AbstractEntity.java
@@ -0,0 +1,10 @@
+package io.edanni.money.domain.entity;
+
+import java.io.Serializable;
+
+/**
+ * Created by eduardo on 23/11/16.
+ */
+public abstract class AbstractEntity implements Serializable
+{
+}
